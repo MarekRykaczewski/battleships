a418f65c8fe23055c9ed69677c454bb6e03a9bfe
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-12-02 16:36:32 +0100

    ensure placeShip does not go out of bounds for Y axis placements

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -27,3 +27,8 @@ test("Do not allow placements that put ship out of bounds (X axis)", () => {
     let gameboard = new Gameboard()
     expect(gameboard.placeShip(ship, 19, "x")).toBe(false)
 })
+test("Do not allow placements that put ship out of bounds (Y axis)", () => {
+    let ship = new Ship([0,1,2])
+    let gameboard = new Gameboard()
+    expect(gameboard.placeShip(ship, 89, "y")).toBe(false)
+})

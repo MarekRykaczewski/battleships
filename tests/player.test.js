2f4b8cde64deea191bb02d5804a24803a19a2d75
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-12-06 19:18:42 +0100

    test placeRandom does not place a ship if numberOfShips is exceeded

--- tests/player.test.js
+++ tests/player.test.js
@@ -13,3 +13,9 @@ test("placeRandom places a ship", () => {
     player1.placeRandom(ship1)
     expect(gameboard1.ships[0].coordinates).toStrictEqual([0,1,2])
 })
+test("placeRandom does not place a ship if exceeds players numberOfShips", () => {
+    let ship1 = new Ship([0,1,2])
+    let gameboard1 = new Gameboard()
+    let player1 = new Player(gameboard1, 0)
+    expect(player1.placeRandom(ship1)).toBe(false)
+})

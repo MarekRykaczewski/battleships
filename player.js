3a0cbad5b63af35b4ee9fc1d00bac166bc05ef4b
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-12-06 19:53:23 +0100

    remove placeRandom

---
+++ player.js
@@ -0,0 +1,11 @@
+class Player {
+    constructor (gameboard, numberOfShips) {
+        this.gameboard = gameboard
+        this.numberOfShips = numberOfShips
+    }
+
+
+}
+
+
+module.exports = {Player: Player} 
--- tests/player.test.js
+++ tests/player.test.js
@@ -4,18 +4,3 @@ const gameboard = require("../gameboard")
 let Gameboard = gameboard.Gameboard
 const ship = require("../ship")
 let Ship = ship.Ship
-
-
-test("placeRandom places a ship", () => {
-    let ship1 = new Ship([0,1,2])
-    let gameboard1 = new Gameboard()
-    let player1 = new Player(gameboard1, 3)
-    player1.placeRandom(ship1)
-    expect(gameboard1.ships[0].coordinates).toStrictEqual([0,1,2])
-})
-test("placeRandom does not place a ship if exceeds players numberOfShips", () => {
-    let ship1 = new Ship([0,1,2])
-    let gameboard1 = new Gameboard()
-    let player1 = new Player(gameboard1, 0)
-    expect(player1.placeRandom(ship1)).toBe(false)
-})

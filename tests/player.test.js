8146923bcef667cc9898c02dd65929d3a0832cd4
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-12-06 19:14:43 +0100

    add test for placeRandom

---
+++ tests/player.test.js
@@ -0,0 +1,15 @@
+const player = require("../player")
+let Player = player.Player
+const gameboard = require("../gameboard")
+let Gameboard = gameboard.Gameboard
+const ship = require("../ship")
+let Ship = ship.Ship
+
+
+test("placeRandom places a ship", () => {
+    let ship1 = new Ship([0,1,2])
+    let gameboard1 = new Gameboard()
+    let player1 = new Player(gameboard1, 3)
+    player1.placeRandom(ship1)
+    expect(gameboard1.ships[0].coordinates).toStrictEqual([0,1,2])
+})

5db825a0b9d790868be43bce2dab88d441a20eef
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-12-08 15:15:12 +0100

    refactor player.js

--- player.js
+++ player.js
@@ -1,11 +1,22 @@
+const gameboard = require("./gameboard")
+let Gameboard = gameboard.Gameboard
+
 class Player {
-    constructor (gameboard, numberOfShips) {
-        this.gameboard = gameboard
-        this.numberOfShips = numberOfShips
+    constructor (name) {
+        this.name = name
+        this.gameboard = new Gameboard()
+        this.ships = []
+    }
+
+    shoot(coordinate, gameboard) {
+        if (gameboard.opponentBoard()[coordinate] === "empty") {
+            gameboard.receiveAttack(coordinate)
+        } else {
+            return false
+        }
     }
 
 
 }
-
 
 module.exports = {Player: Player} 

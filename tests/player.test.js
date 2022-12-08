fbc9808fd7d64b38cf9c01bfeacc5018a95ecbf9
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-12-08 15:14:50 +0100

    refactor tests

--- tests/player.test.js
+++ tests/player.test.js
@@ -4,3 +4,20 @@ const gameboard = require("../gameboard")
 let Gameboard = gameboard.Gameboard
 const ship = require("../ship")
 let Ship = ship.Ship
+
+test("Creates player", () => {
+    let player = new Player("test")
+    expect(player.name).toBe("test")
+})
+test("Shoots gameboard", () => {
+    let player = new Player("test")
+    let gameboard = new Gameboard()
+    player.shoot(5, gameboard)
+    expect(gameboard.grid[5]).toStrictEqual({"hasShip": false, "isShot": true})
+})
+test("Rejects shot if fired at before", () => {
+    let player = new Player("test")
+    let gameboard = new Gameboard()
+    player.shoot(5, gameboard)
+    expect(player.shoot(5, gameboard)).toBe(false)
+})

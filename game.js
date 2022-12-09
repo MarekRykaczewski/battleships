7521213a06dd04a1eff9fe3d9fd8721e5d703d24
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-12-09 17:36:35 +0100

    add game.js

---
+++ game.js
@@ -0,0 +1,32 @@
+const player = require("./player")
+let Player = player.Player
+const gameboard = require("./gameboard")
+let Gameboard = gameboard.Gameboard
+const ship = require("./ship")
+let Ship = ship.Ship
+
+class Game {
+    constructor () {
+        
+    }
+
+    renderBoard() {
+        for (let i = 0; i < 100; i++) {
+            let temp = document.createElement('div');
+            temp.className = "cell"
+            let boardContainer = document.getElementById('board-container')
+            boardContainer.appendChild(temp);
+        }
+    }
+
+    renderOpponentBoard() {
+        for (let i = 0; i < 100; i++) {
+            let temp = document.createElement('div');
+            temp.className = "cell"
+            let boardContainer = document.getElementById('opponent-board-container')
+            boardContainer.appendChild(temp);
+        }
+    }
+}
+
+module.exports = {Game: Game} 

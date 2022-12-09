eb7516cdcc4c25bae3a61c64d6d83f73cc8b6a10
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-12-09 17:36:11 +0100

    add main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const player = require("./player")
+let Player = player.Player
+const gameboard = require("./gameboard")
+let Gameboard = gameboard.Gameboard
+const ship = require("./ship")
+let Ship = ship.Ship
+const game = require("./game")
+let Game = game.Game
+
+const main = new Game()
+let p1 = new Player("human")
+let p2 = new Player("computer")
+
+main.renderBoard()
+main.renderOpponentBoard()

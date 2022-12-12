7e02c95c6776f250fcc16a0aa60109acc8773a05
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-12-12 10:56:30 +0100

    refactor renderShips to work for both players

--- game.js
+++ game.js
@@ -20,21 +20,8 @@ class Game {
         }
     }
 
-    renderShips(player) {
-        let cells = document.getElementById('board-container').getElementsByClassName('cell')
-        for (let i = 0; i < 100; i++) {
-            for (let j = 0; j < player.gameboard.ships.length; j++) {
-                let searchArray = player.gameboard.ships[j].coordinates
-                let searchCell = parseInt(cells.item(i).dataset.coordinate)
-                if (searchArray.includes(searchCell)) {
-                    cells.item(i).classList.add("occupied")
-                }
-            }
-        }
-    }
-
-    renderShipsOpp(player) {
-        let cells = document.getElementById('opponent-board-container').getElementsByClassName('cell')
+    renderShips(player, boardId) {
+        let cells = document.getElementById(boardId).getElementsByClassName('cell')
         for (let i = 0; i < 100; i++) {
             for (let j = 0; j < player.gameboard.ships.length; j++) {
                 let searchArray = player.gameboard.ships[j].coordinates

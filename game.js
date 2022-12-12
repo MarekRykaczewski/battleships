67120cca6af2f1eae8740d6e57f59928cb0d6211
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-12-12 10:50:43 +0100

    refactor renderBoard to work for both players

--- game.js
+++ game.js
@@ -10,28 +10,31 @@ class Game {
         
     }
 
-    renderBoard() {
+    renderBoard(boardId) {
         for (let i = 0; i < 100; i++) {
             let temp = document.createElement('div');
             temp.className = "cell"
             temp.dataset.coordinate = i
-            let boardContainer = document.getElementById('board-container')
-            boardContainer.appendChild(temp);
-        }
-    }
-
-    renderOpponentBoard() {
-        for (let i = 0; i < 100; i++) {
-            let temp = document.createElement('div');
-            temp.className = "cell"
-            temp.dataset.coordinate = i
-            let boardContainer = document.getElementById('opponent-board-container')
+            let boardContainer = document.getElementById(boardId)
             boardContainer.appendChild(temp);
         }
     }
 
     renderShips(player) {
-        let cells = document.getElementsByClassName('cell')
+        let cells = document.getElementById('board-container').getElementsByClassName('cell')
+        for (let i = 0; i < 100; i++) {
+            for (let j = 0; j < player.gameboard.ships.length; j++) {
+                let searchArray = player.gameboard.ships[j].coordinates
+                let searchCell = parseInt(cells.item(i).dataset.coordinate)
+                if (searchArray.includes(searchCell)) {
+                    cells.item(i).classList.add("occupied")
+                }
+            }
+        }
+    }
+
+    renderShipsOpp(player) {
+        let cells = document.getElementById('opponent-board-container').getElementsByClassName('cell')
         for (let i = 0; i < 100; i++) {
             for (let j = 0; j < player.gameboard.ships.length; j++) {
                 let searchArray = player.gameboard.ships[j].coordinates

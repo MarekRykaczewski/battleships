69bcb1e7cbc10abc64db705dc5877df2c71d0acc
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-12-13 19:36:29 +0100

    use placeShip method for ship placement

--- main.js
+++ main.js
@@ -15,23 +15,32 @@ main.renderBoard("board-container")
 main.renderBoard("opponent-board-container")
 
 let p1Patrol = new Ship([0,1])
-let p1Submarine = new Ship([5,6,7])
-let p1Destroyer = new Ship([31,32,33])
-let p1Battleship = new Ship([51,61,71,81])
-let p1Carrier = new Ship([72,73,74,75,76])
+p1Patrol.coordinates = p1.gameboard.placeShip(p1Patrol, 2 , "x")
 
-p1.gameboard.ships.push(p1Patrol, p1Submarine, p1Destroyer, p1Battleship, p1Carrier)
+let p1Submarine = new Ship([0,1,2])
+p1Submarine.coordinates = p1.gameboard.placeShip(p1Submarine, 10 , "y")
 
-let p2Patrol = new Ship([0,10])
-let p2Submarine = new Ship([22,23,24])
-let p2Destroyer = new Ship([33,43,53])
-let p2Battleship = new Ship([13,14,15,16])
-let p2Carrier = new Ship([75,76,77,78,79])
+// let p1Destroyer = new Ship([0,1,2])
+// p1Patrol.coordinates = p1.gameboard.placeShip(p1Patrol, 2 , "x")
 
-p2.gameboard.ships.push(p2Patrol, p2Submarine, p2Destroyer, p2Battleship, p2Carrier )
+// let p1Battleship = new Ship([0,1,2,3])
+// p1Patrol.coordinates = p1.gameboard.placeShip(p1Patrol, 2 , "x")
+
+// let p1Carrier = new Ship([0,1,2,3,4])
+// p1Patrol.coordinates = p1.gameboard.placeShip(p1Patrol, 2 , "x")
+
+let p2Patrol = new Ship([0,1])
+p2Patrol.coordinates = p2.gameboard.placeShip(p2Patrol, 5 , "y")
+
+let p2Submarine = new Ship([0,1,2])
+p2Submarine.coordinates = p2.gameboard.placeShip(p2Submarine, 50 , "x")
+
+// let p2Destroyer = new Ship([0,1,2])
+// let p2Battleship = new Ship([0,1,2,3])
+// let p2Carrier = new Ship([,1,2,3,4])
 
 main.renderShips(p1, "board-container")
 main.renderShips(p2, "opponent-board-container")
 
 main.addCellEventListeners(p1, p2, "opponent-board-container")
-main.addHitEventListners(p2, "opponent-board-container")
+// main.addHitEventListners(p2, "opponent-board-container")

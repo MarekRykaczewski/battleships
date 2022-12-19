03bc6b2d08ad03ea056949b735ba97d1736278a5
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-12-19 17:25:43 +0100

    update method name

--- main.js
+++ main.js
@@ -20,27 +20,13 @@ p1Patrol.coordinates = p1.gameboard.placeShip(p1Patrol, 2 , "x")
 let p1Submarine = new Ship([0,1,2])
 p1Submarine.coordinates = p1.gameboard.placeShip(p1Submarine, 10 , "y")
 
-// let p1Destroyer = new Ship([0,1,2])
-// p1Patrol.coordinates = p1.gameboard.placeShip(p1Patrol, 2 , "x")
-
-// let p1Battleship = new Ship([0,1,2,3])
-// p1Patrol.coordinates = p1.gameboard.placeShip(p1Patrol, 2 , "x")
-
-// let p1Carrier = new Ship([0,1,2,3,4])
-// p1Patrol.coordinates = p1.gameboard.placeShip(p1Patrol, 2 , "x")
-
 let p2Patrol = new Ship([0,1])
 p2Patrol.coordinates = p2.gameboard.placeShip(p2Patrol, 5 , "y")
 
 let p2Submarine = new Ship([0,1,2])
 p2Submarine.coordinates = p2.gameboard.placeShip(p2Submarine, 50 , "x")
 
-// let p2Destroyer = new Ship([0,1,2])
-// let p2Battleship = new Ship([0,1,2,3])
-// let p2Carrier = new Ship([,1,2,3,4])
-
 main.renderShips(p1, "board-container")
 main.renderShips(p2, "opponent-board-container")
 
-main.addCellEventListeners(p1, p2, "opponent-board-container")
-// main.addHitEventListners(p2, "opponent-board-container")
+main.addEnemyCellEventListeners(p1, p2, "opponent-board-container")
